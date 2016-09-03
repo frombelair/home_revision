f530240a5ea132fb27784782d3e9794034b37e8f
frombelair <lbenchouk@gmail.com>
2016-09-03 12:54:30 -0400

    home exercise 9/3

---
+++ revision.rb
@@ -0,0 +1,61 @@
+number = 1
+100.times do
+puts number
+number = number +1
+end
+
+
+ 
+ 100.times do  
+ puts 1            
+ end               
+ 
+number = 1
+100.times do
+	puts number
+	if number == 3
+		puts "mined"
+	else
+		puts number
+	end
+	number = number + 1
+end
+
+number = 1
+100.times do
+	if number == 3
+		puts "mined"
+	elsif 
+		number == 5
+		puts "minds"
+	else
+		puts number
+	end
+	number = number + 1
+end
+number = 1
+100.times do
+	if number % 3 == 0
+		puts "mined"
+	elsif 
+		number % 5 == 0
+		puts "minds"
+	else
+		puts number
+	end
+	number = number + 1
+end
+		
+number = 1
+100.times do
+	if number % 15 == 0
+		puts "minedminds"
+	elsif number % 3 == 0
+		puts"mined"
+	elsif number % 5 == 0
+		puts "minds"
+	else
+		puts number
+	end
+	number = number + 1
+end
